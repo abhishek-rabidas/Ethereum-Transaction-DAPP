544f857b497bc7890fde172d0797862e7e9dcc6e
abhishek <avisec07@gmail.com>
2022-06-07 06:28:37 +0530

    ether transaction feature

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -9,11 +9,19 @@ import org.web3j.protocol.http.HttpService;
 import org.web3j.utils.Convert;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class App {
     public static Web3j web3j = null;
 
     public static void main(String[] args) {
         web3j = Web3j.build(new HttpService("HTTP://127.0.0.1:7545"));
+        WalletServices walletServices = new WalletServices();
+        try {
+            walletServices.sendEthers("961c162a4db6448b681eaae5dbece0a3771188c3688373fca6a2a5e619a43dff", "0x4CA299C91bb5C0cD27F6De7a673bE68d09c3Fd3D", "50", 20,
+                    6721975);
+        } catch (IOException | ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/app/Services/WalletServices.java
+++ src/main/java/app/Services/WalletServices.java
@@ -1,20 +1,55 @@
 package app.Services;
 
 import app.App;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.RawTransaction;
+import org.web3j.crypto.TransactionEncoder;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
 import org.web3j.utils.Convert;
+import org.web3j.utils.Numeric;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
 
 import static app.App.web3j;
 
 public class WalletServices {
 
-    public void checkBalance() throws IOException {
-        EthGetBalance ethGetBalance = web3j.ethGetBalance("0xae3D11BB19ce8Fbe1279041Db209fc70F4C09E9f",
+    public void checkBalance(String address) throws IOException {
+        EthGetBalance ethGetBalance = web3j.ethGetBalance(address,
                 DefaultBlockParameterName.LATEST).send();
 
         System.out.println("Bal: " + Convert.fromWei(ethGetBalance.getBalance().toString(), Convert.Unit.ETHER));
+    }
+
+    public void sendEthers(String senderPrivateKey, String recipientAddress, String amount, int gasPrice, int gasLimit) throws IOException, ExecutionException,
+            InterruptedException {
+        Credentials credentials = Credentials.create(senderPrivateKey);
+
+        System.out.println("Current Balance: "
+                + Convert.fromWei(web3j.ethGetBalance(credentials.getAddress(), DefaultBlockParameterName.LATEST)
+                .send().getBalance().toString(), Convert.Unit.ETHER));
+
+        EthGetTransactionCount ethGetTransactionCount = web3j
+                .ethGetTransactionCount(credentials.getAddress(), DefaultBlockParameterName.LATEST).send();
+        BigInteger nonce = ethGetTransactionCount.getTransactionCount();
+
+        BigInteger value = Convert.toWei(amount, Convert.Unit.ETHER).toBigInteger();
+
+
+        RawTransaction rawTransaction = RawTransaction.createEtherTransaction(nonce, Convert.toWei(String.valueOf(gasPrice), Convert.Unit.GWEI).toBigInteger(), BigInteger.valueOf(gasLimit),
+                recipientAddress, value);
+
+        byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
+        String hexValue = Numeric.toHexString(signedMessage);
+
+        EthSendTransaction ethSendTransaction = web3j.ethSendRawTransaction(hexValue).send();
+        String transactionHash = ethSendTransaction.getTransactionHash();
+        System.out.println("transactionHash: " + transactionHash);
+
     }
 }
